772bdd0876b033bb92f4fce3847ed4d1de854e2f
atef-jlassi <91597099+atef-jlassi@users.noreply.github.com>
2022-11-06 22:13:12 +0100

    update comment

--- chapter 1/ImportExample.java
+++ chapter 1/ImportExample.java
@@ -2,7 +2,7 @@ package a.chapter.one;
 import java.util.Random;
 public class ImportExample {
     public static void main(String[] args) {
-        Random r = new Random(); // DOES NOT COMPILE
+        Random r = new Random(); // DOES NOT COMPILE IF WE REMOVE IMPORTS
         System.out.println(r.nextInt(10));
     }
 }
